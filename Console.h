89f4ec10b803429b06365313bd0290751c32e8fc
19522063 <74546536+khacphuong2503@users.noreply.github.com>
2022-10-15 22:08:56 +0700

    game

---
+++ Console.h
@@ -0,0 +1,87 @@
+﻿#pragma once
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+#define BLACK_COLOR			0
+#define DARK_BLUE_COLOR		1
+#define DARK_GREEN_COLOR	2
+#define DARK_CYAN_COLOR		3
+#define DARK_RED_COLOR		4
+#define DARK_PINK_COLOR		5
+#define DARK_YELLOW_COLOR	6
+#define DARK_WHITE_COLOR	7
+#define GREY_COLOR			8
+#define BLUE_COLOR			9
+#define GREEN_COLOR			10
+#define CYAN_COLOR			11
+#define RED_COLOR			12
+#define PINK_COLOR			13
+#define YELLOW_COLOR		14
+#define WHITE_COLOR			15
+
+#define DOT_RAN 12 	//Hình ảnh của 1 đốt rắn
+#define MAX 100			//Số đốt rắn tối đa
+#define LEN 1			//Mã hướng của rắn
+#define XUONG 2			//Mã hướng của rắn
+#define TRAI 3			//Mã hướng của rắn
+#define PHAI 4			//Mã hướng của rắn
+#define TUONG_TREN 1	//Vị trí của tường
+#define TUONG_DUOI 13	//Vị trí của tường
+#define TUONG_TRAI 3	//Vị trí của tường
+#define TUONG_PHAI 53	//Vị trí của tường
+
+#define KEY_UP		1072
+#define KEY_DOWN	1080
+#define KEY_LEFT	1075
+#define KEY_RIGHT	1077
+#define KEY_NONE	-1
+
+
+//Tọa độ console
+struct ToaDo
+{
+	int x, y;
+
+};
+
+// Lấy nút bàn phím do người dùng bấm
+// Trả về: Mã của phím
+int inputKey();
+
+// Xóa màn hình
+void clrscr();
+
+// Di chuyển con trỏ console đến vị trí có tọa độ (x, y)
+void gotoXY(int x, int y);
+
+// Khởi tạo độ dài và vị trí thân rắn ban đầu
+void KhoiTaoRan();
+
+//Cập nhật tọa độ của các đốt rắn khi rắn di chuyển 1 bước
+ToaDo DiChuyen(int huong);
+
+//Hiển thị rắn theo tọa độ đã được lưu trong mảng
+void HienThiRan(ToaDo x);
+
+//Bắt sự kiện bàn phím để xác định hướng mới
+void BatSuKien(int& huong);
+
+//Hàm dùng để vẽ tường
+void VeTuong();
+
+//Kiển tra những trường hợp thua
+bool KtraThua();
+
+//Xử lí khi game over
+void XuLyThua();
+
+//Hiển thị mồi random
+ToaDo HienThiMoi();
+
+//Kiểm tra rắn đã ăn mồi hay chưa
+bool KtraDaAnMoi(ToaDo moi);
+
+//Thêm 1 đốt mới khi rắn ăn trúng mồi
+void ThemDot();
+
---
+++ Game_Snack.cpp
@@ -0,0 +1,177 @@
+
+#include "Console.h"
+#include <windows.h>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+ToaDo ran[MAX]; //Tọa độ của các đốt rắn
+int SoDot = 3;  //Số lượng phần tử đốt rắn
+
+void main()
+{
+    
+    KhoiTaoRan();
+    int x = 0, y = 0;
+    int huong = PHAI;
+    int diem = 0;
+    VeTuong();
+    ToaDo moi = HienThiMoi();
+    gotoXY((TUONG_PHAI + 4), TUONG_TREN);
+    cout << "Diem: " << 0;
+    void noCursorType();
+
+    //game loop
+    while (1)
+    {
+        ToaDo DotCuoiCu = DiChuyen(huong);
+        HienThiRan(DotCuoiCu);
+        BatSuKien(huong);
+        if (KtraDaAnMoi(moi) == true)
+        {
+        moi = HienThiMoi();
+        ThemDot();
+        diem = diem + 10;
+        gotoXY((TUONG_PHAI + 4), TUONG_TREN);
+        cout << "Diem: " << diem;
+        }
+        if (KtraThua() == true)
+        break;
+        Sleep(200);
+    }
+    XuLyThua();
+}
+
+
+void VeTuong()
+{
+    for (int x = TUONG_TRAI; x <= TUONG_PHAI; x++)
+    {
+        gotoXY(x, TUONG_TREN);
+        cout << (char)220;
+    }
+    for (int y = TUONG_TREN + 1; y <= TUONG_DUOI; y++)
+    {
+        gotoXY(TUONG_TRAI, y);
+        cout << (char)221;
+
+    }
+    for (int x = TUONG_TRAI; x <= TUONG_PHAI; x++)
+    {
+        gotoXY(x, TUONG_DUOI);
+        cout << (char)223;
+    }
+    for (int y = TUONG_TREN + 1; y <= TUONG_DUOI - 1; y++)
+    {
+        gotoXY(TUONG_PHAI, y);
+        cout << (char)222;
+    }
+}
+
+void HienThiRan(ToaDo DotCuoiCu)
+{
+    for (int i = 0; i < SoDot; i++)
+    {
+        gotoXY(ran[i].x, ran[i].y);
+        cout << (char)DOT_RAN;
+    }
+    gotoXY(DotCuoiCu.x, DotCuoiCu.y);
+    cout << " ";
+}
+
+void XuLyThua()
+{
+    Sleep(500);
+    clrscr();
+    cout << "GAME OVER";
+}
+
+ToaDo HienThiMoi()
+{
+    srand(time(NULL));
+    int x = TUONG_TRAI + 1 + rand() % (TUONG_PHAI - TUONG_TRAI - 1);
+    int y = TUONG_TREN + 1 + rand() % (TUONG_DUOI - TUONG_TREN - 1);
+    gotoXY(x, y);
+    cout << "*";
+    return ToaDo{ x,y };
+}
+
+void KhoiTaoRan()
+{
+    ran[0].x = TUONG_TRAI + 3;
+    ran[1].x = TUONG_TRAI + 2;
+    ran[2].x = TUONG_TRAI + 1;
+    ran[0].y = ran[1].y = ran[2].y = TUONG_TREN + 1;
+    
+}
+
+ToaDo DiChuyen(int huong)
+{
+    ToaDo DotCuoiCu = ran[SoDot - 1];
+    for (int i = SoDot - 1; i >= 1; i--)
+    {
+        ran[i] = ran[i - 1];
+    }
+    switch (huong)
+    {
+    case LEN:
+        ran[0].y--;
+        break;
+    case XUONG:
+        ran[0].y++;
+        break;
+    case TRAI:
+        ran[0].x--;
+        break;
+    case PHAI:
+        ran[0].x++;
+        break;
+    }
+    return DotCuoiCu;
+}
+
+void BatSuKien(int& huong)
+{
+    int key = inputKey();
+    if (key == KEY_UP)
+    {
+        huong = LEN;
+    }
+    else if (key == KEY_DOWN)
+    {
+        huong = XUONG;
+    }
+    else if (key == KEY_LEFT)
+    {
+        huong = TRAI;
+    }
+    else if (key == KEY_RIGHT)
+    {
+        huong = PHAI;
+    }
+}
+
+bool KtraThua()
+{
+    if ((ran[0].y == TUONG_TREN) || (ran[0].y == TUONG_DUOI))
+        return true;//thua
+    if ((ran[0].x == TUONG_TRAI) || (ran[0].x == TUONG_PHAI))
+        return true;//thua
+
+}
+
+bool KtraDaAnMoi(ToaDo moi)
+{
+    if (ran[0].x == moi.x && ran[0].y == moi.y)
+    {
+        return true;
+    }
+    else return false;
+}
+
+void ThemDot()
+{
+    ran[SoDot] = ran[SoDot - 1];
+    SoDot++;
+}
